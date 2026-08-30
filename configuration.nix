f5064cda150627c4f858322daa17570887583c50
Petre Razvan <petre.ispir2002@protonmail.com>
2026-08-30 22:31:26 +0300

    changed default vpn location to germany

--- configuration.nix
+++ configuration.nix
@@ -26,7 +26,7 @@
   nix.settings.auto-optimise-store = true;
   boot.tmp.cleanOnBoot = true;
 
-  # extra binary cache — huge overlap with nixpkgs
+  # extra binary cache
   nix.settings.substituters = [ "https://nix-community.cachix.org" ];
   nix.settings.trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
